8a3e90f34701ebd811db72d774e8cb1b23675e9a
unknown <rizkysf94@gmail.com>
2026-08-16 19:19:06 +0700

    CI: 1.2

--- app/__tests__/pages/setting/setting.test.tsx
+++
@@ -1,9 +0,0 @@
-import SettingPage from "@/app/(pages)/setting/page";
-import { render } from "@testing-library/react";
-
-describe("Setting page", () => {
-  it("render Setting page", () => {
-    const settingPage = render(<SettingPage />);
-    expect(settingPage).toMatchSnapshot();
-  });
-});
